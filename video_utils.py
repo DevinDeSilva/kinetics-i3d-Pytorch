3126d0ba26b71bf7ce835732343967d6d5248165
Devin Yasith De Silva <57395782+DevinDeSilva@users.noreply.github.com>
2023-03-25 02:14:38 -0700

    adding utils required
    Adding video processing utils

---
+++ video_utils.py
@@ -0,0 +1,58 @@
+def read_video_cv2(file_path):
+    cap = cv2.VideoCapture(file_path)
+    all = []
+    i = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        arr = np.array(frame)
+        all.append(arr)
+        i += 1
+    return np.array(all)
+
+class CenterCrop(object):
+    """Crops the given seq Images at the center.
+    Args:
+        size (sequence or int): Desired output size of the crop. If size is an
+            int instead of sequence like (h, w), a square crop (size, size) is
+            made.
+    """
+
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+
+    def __call__(self, imgs):
+        """
+        Args:
+            img (PIL Image): Image to be cropped.
+        Returns:
+            PIL Image: Cropped image.
+        """
+        t, h, w, c = imgs.shape
+        th, tw = self.size
+        i = int(np.round((h - th) / 2.))
+        j = int(np.round((w - tw) / 2.))
+
+        return imgs[:, i:i+th, j:j+tw, :]
+
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(size={0})'.format(self.size)
+
+def read_video_sk(file_path):
+    videodata = skvideo.io.vread(file_path)  
+    return videodata
+
+def video_to_tensor(pic):
+    """Convert a ``numpy.ndarray`` to tensor.
+    Converts a numpy.ndarray (T x H x W x C)
+    to a torch.FloatTensor of shape (C x T x H x W)
+    
+    Args:
+         pic (numpy.ndarray): Video to be converted to tensor.
+    Returns:
+         Tensor: Converted video.
+    """
+    return torch.from_numpy(pic.transpose([3,0,1,2]))
